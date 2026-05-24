43cec046be95260457725305eb2caef00c4873e1
Kamlesh Duble <kamlesh.duble@gmail.com>
2026-05-24 22:54:02 +0800

    FIrst Commit

---
+++ welcome_message.py
@@ -0,0 +1,9 @@
+line1 = '***********************************'
+line2 = '**                               **'
+line3 = '    Welcome to Python Learning     '
+
+print('line1')
+print('line2')
+print('line3')
+print('line2')
+print('line1')
